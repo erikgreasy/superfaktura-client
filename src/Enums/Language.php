63feff4568bcb2685a934de18894122c2bd145cb
Erik Masny <erik.masny@gmail.com>
2023-03-17 23:09:42 +0100

    feat: create enum for languages

---
+++ src/Enums/Language.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Erikgreasy\Superfaktura\Enums;
+
+enum Language: string
+{
+    case Czech = 'cze';
+    case German = 'deu';
+    case English = 'eng';
+    case Croatian = 'hrv';
+    case Hungarian = 'hun';
+    case Italian = 'ita';
+    case Dutch = 'nld';
+    case Polish = 'pol';
+    case Romanian = 'rom';
+    case Russian = 'rus';
+    case Slovak = 'slo';
+    case Slovenian = 'slv';
+    case Spanish = 'spa';
+    case Ukranian = 'ukr';
+}
