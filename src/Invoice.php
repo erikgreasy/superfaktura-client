81265eeb5369f9835bdd0543ef9724da2cb2e73d
Erik Masny <erik.masny@gmail.com>
2023-03-22 12:46:38 +0100

    feat: add remaining fields and setters for invoice

--- src/Invoice.php
+++ src/Invoice.php
@@ -7,9 +7,19 @@ class Invoice
     private array $invoice = [];
     private array $items = [];
     private array $client = [];
+    private array $myData = [];
+    private array $invoiceSetting = [];
+    private array $invoiceExtra = [];
 
     public function __construct(protected Superfaktura $superfaktura)
     {
+    }
+
+    public function setInvoice(array $invoice): self
+    {
+        $this->invoice = $invoice;
+
+        return $this;
     }
 
     public function setClient(array $client): self
@@ -25,6 +35,27 @@ class Invoice
 
         return $this;
     }
+    
+    public function setMyData(array $myData): self
+    {
+        $this->myData = $myData;
+
+        return $this;
+    }
+
+    public function setInvoiceExtra(array $invoiceExtra): self
+    {
+        $this->invoiceExtra = $invoiceExtra;
+
+        return $this;
+    }
+
+    public function setInvoiceSetting(array $invoiceSetting): self
+    {
+        $this->invoiceSetting = $invoiceSetting;
+
+        return $this;
+    }
 
     public function save(): array
     {
@@ -32,6 +63,9 @@ class Invoice
             'Invoice' => $this->invoice,
             'InvoiceItem' => $this->items,
             'Client' => $this->client,
+            'MyData' => $this->myData,
+            'InvoiceSetting' => $this->invoiceSetting,
+            'InvoiceExtra' => $this->invoiceExtra,
         ]);
     }
 }
